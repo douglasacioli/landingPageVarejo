f49e22eb9c994f8ee7beb2aba6b220e6d5347d9e
Doug <douglas.acioli@gmail.com>
2022-08-31 01:54:09 -0300

    operações de crud implementadas

--- LandingPageVarejo.API/LandingPageVarejo.API/Controllers/LeadController.cs
+++ LandingPageVarejo.API/LandingPageVarejo.API/Controllers/LeadController.cs
@@ -1,4 +1,5 @@
-﻿using LandingPageVarejo.API.Models;
+﻿using LandingPageVarejo.API.Data;
+using LandingPageVarejo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,33 +10,22 @@ namespace LandingPageVarejo.API.Controllers
     [Route("[Controller]")]
     public class LeadController : ControllerBase
     {
-        public static int Id = 1;
-        private static IEnumerable<Lead> leads = new List<Lead>()
+        private LeadContext _context;
+        public LeadController(LeadContext context)
         {
-            new Lead()
-            {
-                Id = 1,
-                Nome = "Douglas",
-                Email = "douglas.acioli@gmail.com"
-            },
-             new Lead()
-            {
-                Id = 2,
-                Nome = "Arthur",
-                Email = "arthur.acioli@gmail.com"
-            },
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public IEnumerable<Lead> RecuperarFilmes()
         {
-            return leads;
+            return _context.Leads;
         }
 
         [HttpGet("{id}")]
        public IActionResult ObterLeadPorId(int id)
         {
-            Lead lead = leads.FirstOrDefault(lead => lead.Id == id);
+            Lead lead = _context.Leads.FirstOrDefault(lead => lead.Id == id);
             if(lead != null)
             {
                 return Ok(lead);
@@ -46,8 +36,35 @@ namespace LandingPageVarejo.API.Controllers
         [HttpPost]
         public IActionResult AdicionarLead([FromBody] Lead lead)
         {
-          
+            _context.Leads.Add(lead);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(ObterLeadPorId), new { id = lead.Id }, lead);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarLead(int id, [FromBody] Lead novolead)
+        {
+            Lead lead = _context.Leads.FirstOrDefault(lead => lead.Id == id);
+            if(lead == null)
+            {
+                return NotFound();
+            }
+            lead.Nome = novolead.Nome;
+            lead.Email = novolead.Email;
+            _context.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult ExcluirLead(int id)
+        {
+            Lead lead = _context.Leads.FirstOrDefault(lead => lead.Id == id);
+            if(lead == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(lead);
+            _context.SaveChanges();
+            return NoContent();
         }
 
     }
