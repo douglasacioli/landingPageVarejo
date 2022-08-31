9f1a79806fff674a11ab708c7c5b0e8a79b0b7c4
Doug <douglas.acioli@gmail.com>
2022-08-31 00:45:02 -0300

    criação da model lead com validações utilizando DataNotations, -criação da LeadController com rotas configuradas para atender as requisições de obter, adicionar e obter por id , para a manipulação dos dados fiz uso de uma lista genérica

---
+++ LandingPageVarejo.API/LandingPageVarejo.API/Controllers/LeadController.cs
@@ -0,0 +1,54 @@
+﻿using LandingPageVarejo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LandingPageVarejo.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class LeadController : ControllerBase
+    {
+        public static int Id = 1;
+        private static IEnumerable<Lead> leads = new List<Lead>()
+        {
+            new Lead()
+            {
+                Id = 1,
+                Nome = "Douglas",
+                Email = "douglas.acioli@gmail.com"
+            },
+             new Lead()
+            {
+                Id = 2,
+                Nome = "Arthur",
+                Email = "arthur.acioli@gmail.com"
+            },
+        };
+
+        [HttpGet]
+        public IEnumerable<Lead> RecuperarFilmes()
+        {
+            return leads;
+        }
+
+        [HttpGet("{id}")]
+       public IActionResult ObterLeadPorId(int id)
+        {
+            Lead lead = leads.FirstOrDefault(lead => lead.Id == id);
+            if(lead != null)
+            {
+                return Ok(lead);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarLead([FromBody] Lead lead)
+        {
+          
+            return CreatedAtAction(nameof(ObterLeadPorId), new { id = lead.Id }, lead);
+        }
+
+    }
+}
---
+++ LandingPageVarejo.API/LandingPageVarejo.API/Models/Lead.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LandingPageVarejo.API.Models
+{
+    public class Lead
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="campo nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        public string Email { get; set; }
+    }
+}
